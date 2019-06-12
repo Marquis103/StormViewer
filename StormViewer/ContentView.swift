0a6f0a1e42d3ed43a81064b1a6b0eeff6cf91bfd
Marquis Dennis <marquis103@gmail.com>
2019-06-12 01:20:02 -0400

    Sample storm viewer app using swiftui

--- StormViewer/ContentView.swift
+++ StormViewer/ContentView.swift
@@ -7,10 +7,62 @@
 //
 
 import SwiftUI
+import Combine
+
+class ImageDataSource: BindableObject {
+    let didChange = PassthroughSubject<Void, Never>()
+    fileprivate var images = [String]()
+    
+    init() {
+        let fm = FileManager.default
+        
+        if let path = Bundle.main.resourcePath, let imageItems = try? fm.contentsOfDirectory(atPath: path) {
+            for image in imageItems where image.hasPrefix("nssl") {
+                images.append(image)
+            }
+        }
+        
+        didChange.send(())
+    }
+}
+
+struct ImageDetailView: View {
+    @State private var hidesNavigationBar = false
+    var image: String
+    
+    var body: some View {
+        let img = UIImage(named: image)!
+        return Image(uiImage: img)
+            .resizable()
+            .aspectRatio(1024/768, contentMode: .fit)
+            .navigationBarTitle(Text(image), displayMode: .inline)
+            .navigationBarHidden(hidesNavigationBar)
+            .tapAction {
+                self.hidesNavigationBar.toggle()
+                
+        }
+    }
+}
 
 struct ContentView : View {
+    @ObjectBinding var imageDataSource = ImageDataSource()
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List(imageDataSource.images.identified(by: \.self)) { image in
+                NavigationButton(destination: ImageDetailView(image: image), isDetail: true) {
+                    HStack {
+                        Image(uiImage: UIImage(named: image)!)
+                            .resizable()
+                            .frame(width: 100, height: 100, alignment: .leading)
+                            .cornerRadius(50)
+                        Text(image)
+                    }
+                    
+                }
+                }
+                .navigationBarTitle(Text("Storm Viewer"))
+        }
     }
 }
 
